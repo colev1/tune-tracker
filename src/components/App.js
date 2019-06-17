be4611c0ec930951fefbbec87d9dba9ffb6ddde9
colev1 <colevanacore@gmail.com>
2019-06-17 11:46:31 -0400

    Setup main component to hold all tracks

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { connect } from 'react-redux'
 import Search from '../containers/Search';
 import Header from '../containers/Header';
+import Main from '../containers/Main';
+
 
 
 // import { simpleAction } from './actions/simpleAction';
